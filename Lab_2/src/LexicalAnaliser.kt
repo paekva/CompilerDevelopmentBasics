ac4abbb8bdbca2e9b54ee40806efb66c388cc347
Kate Pavlova <paekva@yandex.ru>
2019-04-09 18:54:51 +0300

    added uni minus sign

--- Lab_2/src/LexicalAnaliser.kt
+++ Lab_2/src/LexicalAnaliser.kt
@@ -1,91 +1,102 @@
-class LexicalAnaliser(private val file: List<String>){
-    private val lexemTable: ArrayList<Lexem> = arrayListOf()
+class LexicalAnaliser(private val file: List<String>) {
+    val lexemTable: ArrayList<Lexem> = arrayListOf()
+    val indentTable: ArrayList<Lexem> = arrayListOf()
     val lineLexemCount: ArrayList<Int> = arrayListOf()
 
-    fun analise(): ArrayList<Lexem>{
-        file.forEachIndexed{index, line ->
+    fun analise() {
+        file.forEachIndexed { index, line ->
             lineAnaliser(line, index)
 
             var tmp = 0
-            lineLexemCount.forEach{ tmp += it}
+            lineLexemCount.forEach { tmp += it }
             lineLexemCount.add(tmp + lexemTable.size - 1)
         }
-        return lexemTable
     }
 
-    private fun lineAnaliser(line: String, lineIndex: Int){
+    private fun lineAnaliser(line: String, lineIndex: Int) {
         var lexem = ""
         var index = 0
 
-        while(index < line.length){
+        while (index < line.length) {
             val c = line[index]
             val isEndOfLine = index == line.length - 1
             val isSpaceSymbol = c == ' '
-            val isBeginOfAComment = !isEndOfLine && c == '/' && line[index+1]=='/'
+            val isBeginOfAComment = !isEndOfLine && c == '/' && line[index + 1] == '/'
 
-            if(isBeginOfAComment)
+            if (isBeginOfAComment)
                 break
 
 
-            lexem+=c
+            lexem += c
             val type = recognise(lexem)
-            val typeOfBiggerLexem = if(!isEndOfLine) recognise(lexem + line[index + 1]) else LexemType.UNRECOGNISED
+            val typeOfBiggerLexem = if (!isEndOfLine) recognise(lexem + line[index + 1]) else LexemType.UNRECOGNISED
 
             try {
-                if(typeOfBiggerLexem != LexemType.UNRECOGNISED) {
+                if (typeOfBiggerLexem != LexemType.UNRECOGNISED) {
                     index++
                     continue
-                }
-                else if(type != LexemType.UNRECOGNISED ){
-                    addToTable(lexem, type)
+                } else if (type != LexemType.UNRECOGNISED) {
+                    addToLexemTable(lexem, type)
                     lexem = ""
-                }
-                else if (isSpaceSymbol)
-                    lexem=""
+                } else if (isSpaceSymbol)
+                    lexem = ""
                 else throw Exception()
-            }
-            catch(e: Exception){
-                println("Error in parsing expression '$lexem' at position ${lineIndex+1} : ${index+1}")
+            } catch (e: Exception) {
+                println("Error in parsing expression '$lexem' at position ${lineIndex + 1} : ${index + 1}")
             }
 
             index++
         }
     }
 
-    private fun recognise(lexem: String): LexemType{
-        if(Words.values().map { it.lexem }.contains(lexem))
+    private fun recognise(lexem: String): LexemType {
+        if (Words.values().map { it.lexem }.contains(lexem))
             return LexemType.WORD
 
-        if(MathOperators.values().map { it.lexem }.contains(lexem))
-            return LexemType.MATH_OPERATOR
+        if (UniMathOperators.values().map { it.lexem }.contains(lexem)
+            && lexemTable[lexemTable.size - 1].type != LexemType.IDENTIFIER && lexemTable[lexemTable.size - 1].type != LexemType.CONST
+        )
+            return LexemType.UNI_MATH_OPERATOR
 
-        if(Relations.values().map { it.lexem }.contains(lexem))
+        if (BinMathOperators.values().map { it.lexem }.contains(lexem))
+            return LexemType.BIN_MATH_OPERATOR
+
+        if (Relations.values().map { it.lexem }.contains(lexem))
             return LexemType.RELATION_OPERATOR
 
-        if(lexem.matches("^[0-9]+".toRegex()))
+        if (lexem.matches("^[0-9]+".toRegex()))
             return LexemType.CONST
 
-        if(lexem.matches("^[a-zA-Z]+".toRegex()))
+        if (lexem.matches("^[a-zA-Z]+".toRegex()))
             return LexemType.IDENTIFIER
 
         return LexemType.UNRECOGNISED
     }
 
-    private fun addToTable(lexem: String, type: LexemType){
-        lexemTable.add(Lexem(type,lexem))
+    private fun addToLexemTable(lexem: String, type: LexemType) {
+        lexemTable.add(Lexem(type, lexem))
+
+        if (type == LexemType.IDENTIFIER || type == LexemType.CONST)
+            addToIdentTable(lexem, type)
+    }
+
+    private fun addToIdentTable(lexem: String, type: LexemType) {
+        val newLexem = Lexem(type, lexem)
+        if(!indentTable.map { it.value }.contains(newLexem.value))
+            indentTable.add(newLexem)
     }
 }
-
 
 class Lexem(val type: LexemType, val value: String)
 
 enum class LexemType(val code: Int){
     RELATION_OPERATOR(0),
-    MATH_OPERATOR(1),
-    WORD(2),
-    IDENTIFIER(3),
-    CONST(4),
-    UNRECOGNISED(5)
+    BIN_MATH_OPERATOR(1),
+    UNI_MATH_OPERATOR(2),
+    WORD(3),
+    IDENTIFIER(4),
+    CONST(5),
+    UNRECOGNISED(6)
 }
 
 enum class Words(val lexem: String) {
@@ -108,9 +119,13 @@ enum class Relations(val lexem: String) {
     Equal("=")
 }
 
-enum class MathOperators(val lexem: String){
+enum class BinMathOperators(val lexem: String){
     Minus("-"),
     Plus("+"),
     Mul("*"),
     Div("/"),
+}
+
+enum class UniMathOperators(val lexem: String){
+    Minus("-"),
 }
--- Lab_2/src/Main.kt
+++ Lab_2/src/Main.kt
@@ -3,21 +3,18 @@ import java.io.File
 fun main(){
     val file = readFile("E:/in.txt")
     val lexicalAnaliser = LexicalAnaliser(file)
-    val table = lexicalAnaliser.analise()
-    writeToFile(table, lexicalAnaliser.lineLexemCount, "E:/out.txt" )
+    lexicalAnaliser.analise()
+    writeToFile(lexicalAnaliser.lexemTable, "E:/lexemTable.txt" )
+    writeToFile(lexicalAnaliser.indentTable, "E:/indentTable.txt" )
 }
 
 fun readFile(fileName: String): List<String>
         = File(fileName).readLines(Charsets.UTF_8)
 
-fun writeToFile(table: ArrayList<Lexem>, lineNumbers: ArrayList<Int>, fileName: String){
-    var line = 0
-    val myfile = File(fileName)
-
-    myfile.printWriter().use { out ->
+fun writeToFile(table: ArrayList<Lexem>, fileName: String){
+    File(fileName).printWriter().use { out ->
         table.forEachIndexed{ index, it ->
-            if (lineNumbers[line] < index) line ++
-            out.println("line:$line <${it.type},${it.value}>")
+            out.println("<${it.type.code},${it.value}>")
         }
     }
 }
